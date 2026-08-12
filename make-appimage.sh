42274ffe3b72e2ba8cc2794fdc30babce12a1ee8
Blackfox-Thane <107892323+Blackfox-Thane@users.noreply.github.com>
2026-08-12 19:26:16 -0400

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,9 +8,10 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=https://raw.githubusercontent.com/UZDoom/UZDoom/refs/heads/trunk/src/posix/freedesktop/org.zdoom.UZDoom.svg
+export DESKTOP=https://raw.githubusercontent.com/UZDoom/UZDoom/refs/heads/trunk/src/posix/freedesktop/org.zdoom.UZDoom.desktop
 export DEPLOY_VULKAN=1
+export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/uzdoom
