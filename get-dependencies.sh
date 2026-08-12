c87918a5d548d6d5171d8830a98586bf120f8cec
Blackfox-Thane <107892323+Blackfox-Thane@users.noreply.github.com>
2026-08-12 19:28:10 -0400

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -19,34 +19,9 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+make-aur-package uzdoom
 
 # If the application needs to be manually built that has to be done down here
-echo "Making UZDoom..."
-echo "---------------------------------------------------------------"
-DIR="UZDoom"
-REPO_URL="https://github.com/UZDoom/UZDoom.git"
-
-if [ -d "$DIR" ]; then
-  cd "$DIR" && git pull
-else
-  git clone "$REPO_URL" "$DIR"
-fi
-
-if [ -d "$DIR" ]; then
-  cd "$DIR/build"
-else
-  mkdir -p $DIR/build
-fi
-
-cmake                                \
-  -DCMAKE_BUILD_TYPE=RelWithDebInfo  \
-  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-  -DBUILD_SHARED_LIBS=OFF            \
-  -G Ninja                           \
-  ..
-
-cmake --build .
 
 # if you also have to make nightly releases check for DEVEL_RELEASE = 1
 #
