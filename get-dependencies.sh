40b5803433e7c7d8bb16913ecb2cfa23ee265aeb
Blackfox-Thane <107892323+Blackfox-Thane@users.noreply.github.com>
2026-08-12 20:19:10 -0400

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm
+pacman -Syu --noconfirm \
   openmp 		\
   openal 		\
   sdl2     	\
